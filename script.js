af0c1e6c48fcda9032f2d847e72ffe32d7116731
robertolagreca <robertolagreca92@gmail.com>
2022-11-16 15:09:21 +0100

    Funzione pari e dispari ver.1

--- script.js
+++ script.js
@@ -51,3 +51,80 @@ function palindromoCheck(p) {
     return false;   
   }
   
+
+//Secondo esercizio
+
+//Pari e Dispari:
+//L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+//Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+//Sommiamo i due numeri
+//Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+//Dichiariamo chi ha vinto.
+
+
+//Inizializzo variabili globali. Risultato lo imposto a false.
+let sceltaUtente, numeroEstratto, risultato;
+risultato = false;
+
+
+//chiedo all'utente di scegliere tra pari e dispari. chiedo di inserire un numero tra 1 e 5.
+//Stampo in console.
+sceltaUtente = prompt("Scegli pari o dispari.");
+numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 5"));
+console.log("L'utente ha scelto " + sceltaUtente + " ed ha inserito il numero " + numeroUtente);
+
+
+//richiamo la funzione numeroRan per calcolare un numero random da 1 a 5.
+//dopo di che passo alla funzione controlloSomma due argomenti: 
+//il numero scelto dall'utente e quello estratto.
+numeroEstratto = parseInt(numeroRan());
+risultato = controlloSomma(numeroUtente , numeroEstratto);
+
+
+//if con tutti i casi in cui vengono confrontati:
+//se la variabile risultato è true o false (se true significa che la somma è pari, false è dispari)
+
+if(risultato && sceltaUtente == "pari"){
+
+    console.log("Hai vinto. Il numero calcolato è pari!");
+
+} else if (risultato && sceltaUtente !="pari"){
+
+    console.log("Hai perso. Il numero calcolato è dispari!");
+
+} else if (!risultato && sceltaUtente == "dispari"){
+    console.log("Hai vinto. Il numero calcolato è dispari!");
+
+} else {
+    console.log("Hai perso. Il numero calcolato è pari");
+}
+
+
+
+function numeroRan(){
+    let numero = 0;
+
+    numero = Math.floor(Math.random() * 6);
+    console.log("Il numero estratto è " + numero);
+
+    return numero;
+}
+
+
+
+function controlloSomma(numUtente, numEstratto){
+    let somma, controllo;
+    somma = 0;
+    controllo = "";
+
+    somma = numUtente + numEstratto;
+    console.log("La somma dei due numeri è " + somma);
+
+    if(somma % 2 == 0){
+        controllo = "pari";
+        return true;
+    } else {
+        controllo = "dispari";
+        return false;
+    }
+}
